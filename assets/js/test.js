58e2310387ddb13d22ca2683aa9357f547d0a30b
julianflancheros <jflancherosm@unal.edu.co>
2025-04-19 02:39:31 -0500

    upload new page for all the projects, load the information dynamically

---
+++ assets/js/test.js
@@ -0,0 +1,453 @@
+const portfolioDetails = {
+    title: "Portfolio Details",
+    'tpi-website': {
+        description: "TPi website",
+        summary: [
+            { client: "Subject Taller de proyectos Interdiciplinarios of Enginnering Faculty" },
+            { date: "From February to August, 2021" },
+            { url: "https://ingenieria.bogota.unal.edu.co/tpi/"}
+        ],
+        projectDescription: `
+            This productive farm project was divided in three parts:
+            <ol>
+              <li>Location the piece of land to build the house</li>
+              <li>The house design</li>
+              <li>The productive system design of the house</li>
+            </ol>
+            For this project we designed from the preparation of the land to the irrigation systems for each crop, we did it following the parameters established in the guide.<br><br>
+            Our design was born as a compliment to beauty possessed by colonial houses, preserving the simplicity and elegance that this type of construction pose. The house is structured on a foundation surface of some isolated footings and foundation beams, allowing it to be earthquake resistant, a reinforcement was made in the stair area as it was considered that it could be a area in which there will be a lot of movement and we wanted guarantee its durability.<br><br>
+            To develop this project we had to use some Autodesk software (AutoCAD, REVIT and CIVIL 3D) as well as geographic information capture software (ArcGIS, QGIS and GlobalMapper), for the creation of three-dimensional objects we used Sketchup software and to divide the work we design work schedules using Visual Basic from Excel.
+        `,
+        media: {
+            type: "image",
+            items: [
+                {
+                    src: "assets/img/portfolio/portfolio-7-details-1.jpg",
+                    alt: ""
+                },
+                {
+                    src: "assets/img/portfolio/portfolio-7-details-2.jpg",
+                    alt: ""
+                },
+                {
+                    src: "assets/img/portfolio/portfolio-7-details-3.jpg",
+                    alt: ""
+                },
+                {
+                    src: "assets/img/portfolio/portfolio-7-details-4.jpg",
+                    alt: ""
+                },
+                {
+                    src: "assets/img/portfolio/portfolio-7-details-5.jpg",
+                    alt: ""
+                }
+            ]
+        },
+        skills: [
+            "AutoCAD",
+            "REVIT",
+            "CIVIL 3D",
+            "ArcGIS",
+            "QGIS",
+            "GlobalMapper",
+            "Sketchup",
+            "Visual Basic"
+        ]
+    },
+    'dynamic-maps': {
+        description: "Dynamic Maps",
+        summary: [
+            { client: "Subject Taller de proyectos Interdiciplinarios of Enginnering Faculty" },
+            { date: "From February to August, 2021" },
+            { url: "https://ingenieria.bogota.unal.edu.co/tpi/"}
+        ],
+        projectDescription: `
+            This productive farm project was divided in three parts:
+            <ol>
+              <li>Location the piece of land to build the house</li>
+              <li>The house design</li>
+              <li>The productive system design of the house</li>
+            </ol>
+            For this project we designed from the preparation of the land to the irrigation systems for each crop, we did it following the parameters established in the guide.<br><br>
+            Our design was born as a compliment to beauty possessed by colonial houses, preserving the simplicity and elegance that this type of construction pose. The house is structured on a foundation surface of some isolated footings and foundation beams, allowing it to be earthquake resistant, a reinforcement was made in the stair area as it was considered that it could be a area in which there will be a lot of movement and we wanted guarantee its durability.<br><br>
+            To develop this project we had to use some Autodesk software (AutoCAD, REVIT and CIVIL 3D) as well as geographic information capture software (ArcGIS, QGIS and GlobalMapper), for the creation of three-dimensional objects we used Sketchup software and to divide the work we design work schedules using Visual Basic from Excel.
+        `,
+        skills: [
+            "AutoCAD",
+            "REVIT",
+            "CIVIL 3D",
+            "ArcGIS",
+            "QGIS",
+            "GlobalMapper",
+            "",
+            "Visual Basic"
+        ],
+        media: {
+            type: "url",
+            items: [
+                {
+                    src: `<div class="row mapUpper" >
+            <iframe id="qv" src="./Mapas%203D/Modelo%20de%20Elevaciòn%20del%20terreno/Modelo3DNilo.html" frameborder="0"></iframe>
+          </div>`,
+                },
+                {
+                    src: `<div class="row mapUpper">
+              <iframe id="qv" src="./Mapas%203D/Mapa%20Compuesto/Modelo3DNilo.html" frameborder="0"></iframe>
+          </div>`,
+                },
+                {
+                    src: `<div class="row">
+            <iframe id="qv" src="./Mapas%203D/Mapa%20final%20proyecto/modelo.html" frameborder="0"></iframe>
+        </div>`,
+                },
+            ]
+        },
+    },
+    'itawa-website': {
+        description: "Itawa website",
+        summary: [
+            { client: "ITAWA BAND" },
+            { date: "From June to November, 2017" },
+            { url: ""}
+        ],
+        projectDescription: `
+            This web page is designed with responsive web design and custom-designed. It helped the ITAWA BAND think about it presence online. I am excited about this web page, and I'm happy about the results.
+        `,
+        media: {
+            type: "image",
+            items: [
+                {
+                    src: "assets/img/portfolio/portfolio-1-details-1.jpg",
+                    alt: ""
+                },
+                {
+                    src: "assets/img/portfolio/portfolio-1-details-2.jpg",
+                    alt: ""
+                },
+                {
+                    src: "assets/img/portfolio/portfolio-1-details-3.jpg",
+                    alt: ""
+                },
+                {
+                    src: "assets/img/portfolio/portfolio-1-details-4.jpg",
+                    alt: ""
+                },
+                {
+                    src: "assets/img/portfolio/portfolio-1-details-5.jpg",
+                    alt: ""
+                },
+                {
+                    src: "assets/img/portfolio/portfolio-1-details-6.jpg",
+                    alt: ""
+                }
+            ]
+        },
+        skills: [
+            "HTML",
+            "CSS",
+            "Javascript",
+            "Frontend Basic Principies",
+            "Agile Methodologies"
+        ]
+    },
+    'teatroTierra-website': {
+        description: "Teatro Tierra website",
+        summary: [
+            { client: "Claudia Sanchez " },
+            { date: "From June 2024 to Actually" },
+            { url: "https://teatrotierra.org/"}
+        ],
+        projectDescription: `
+            This web page is designed web design and custom-designed. It helped the ITAWA BAND think about it presence online. I am excited about this web page, and I'm happy about the results.
+        `,
+        media: {
+            type: "image",
+            items: [
+                {
+                    src: "assets/img/portfolio/portfolio-1-details-1.jpg",
+                    alt: ""
+                },
+                {
+                    src: "assets/img/portfolio/portfolio-1-details-2.jpg",
+                    alt: ""
+                },
+                {
+                    src: "assets/img/portfolio/portfolio-1-details-3.jpg",
+                    alt: ""
+                },
+                {
+                    src: "assets/img/portfolio/portfolio-1-details-4.jpg",
+                    alt: ""
+                },
+                {
+                    src: "assets/img/portfolio/portfolio-1-details-5.jpg",
+                    alt: ""
+                },
+                {
+                    src: "assets/img/portfolio/portfolio-1-details-6.jpg",
+                    alt: ""
+                }
+            ]
+        },
+        skills: [
+            "Firebase",
+            "SASS",
+            "React.js",
+            "Frontend Basic Principies",
+            "Agile Methodologies"
+        ]
+    },
+    'ramaUnal-website': {
+        description: "Rama Unal website",
+        summary: [
+            { client: "IEEE Universidad Nacional de Colombia" },
+            { date: "From June 2022 to Actually" },
+            { url: "https://ieeeunal.github.io/Pagina_Web/"}
+        ],
+        projectDescription: `
+            This web page is designed web design and custom-designed. It helped the ITAWA BAND think about it presence online. I am excited about this web page, and I'm happy about the results.
+        `,
+        media: {
+            type: "image",
+            items: [
+                {
+                    src: "assets/img/portfolio/portfolio-1-details-1.jpg",
+                    alt: ""
+                },
+                {
+                    src: "assets/img/portfolio/portfolio-1-details-2.jpg",
+                    alt: ""
+                },
+                {
+                    src: "assets/img/portfolio/portfolio-1-details-3.jpg",
+                    alt: ""
+                },
+                {
+                    src: "assets/img/portfolio/portfolio-1-details-4.jpg",
+                    alt: ""
+                },
+                {
+                    src: "assets/img/portfolio/portfolio-1-details-5.jpg",
+                    alt: ""
+                },
+                {
+                    src: "assets/img/portfolio/portfolio-1-details-6.jpg",
+                    alt: ""
+                }
+            ]
+        },
+        skills: [
+            "Node.js",
+            "React.js",
+            "Figma",
+            "MongoDB",
+            "Git",
+            "Social Media management",
+            "Agile Methodologies",
+            "Project management"
+        ]
+    },
+    'catedraJEG-website': {
+        description: "Catradra JEG website",
+        summary: [
+            { client: "Universidad Nacional de Colombia" },
+            { date: "From March to July, 2022" },
+            { url: "http://www.catedras-bogota.unal.edu.co/catedra-jorge-eliecer-gaitan/2022-1"}
+        ],
+        projectDescription: `
+            This web page is designed web design and custom-designed. It helped the ITAWA BAND think about it presence online. I am excited about this web page, and I'm happy about the results.
+        `,
+        media: {
+            type: "image",
+            items: [
+                {
+                    src: "assets/img/portfolio/portfolio-1-details-1.jpg",
+                    alt: ""
+                },
+                {
+                    src: "assets/img/portfolio/portfolio-1-details-2.jpg",
+                    alt: ""
+                },
+                {
+                    src: "assets/img/portfolio/portfolio-1-details-3.jpg",
+                    alt: ""
+                },
+                {
+                    src: "assets/img/portfolio/portfolio-1-details-4.jpg",
+                    alt: ""
+                },
+                {
+                    src: "assets/img/portfolio/portfolio-1-details-5.jpg",
+                    alt: ""
+                },
+                {
+                    src: "assets/img/portfolio/portfolio-1-details-6.jpg",
+                    alt: ""
+                }
+            ]
+        },
+        skills: [
+            "HTML",
+            "CSS",
+            "Javascript",
+            "Joomla",
+            "Figma",
+            "illustrator",
+            "Google Scripts",
+            "Google Workspace",
+        ]
+    },
+    'innovateUN-website': {
+        description: "Innovate UN website",
+        summary: [
+            { client: "Universidad Nacional de Colombia" },
+            { date: "From August to December, 2021" },
+            { url: "https://ingenieria.unal.edu.co/innovate"}
+        ],
+        projectDescription: `
+            This web page is designed web design and custom-designed. It helped the ITAWA BAND think about it presence online. I am excited about this web page, and I'm happy about the results.
+        `,
+        media: {
+            type: "image",
+            items: [
+                {
+                    src: "assets/img/portfolio/portfolio-1-details-1.jpg",
+                    alt: ""
+                },
+                {
+                    src: "assets/img/portfolio/portfolio-1-details-2.jpg",
+                    alt: ""
+                },
+                {
+                    src: "assets/img/portfolio/portfolio-1-details-3.jpg",
+                    alt: ""
+                },
+                {
+                    src: "assets/img/portfolio/portfolio-1-details-4.jpg",
+                    alt: ""
+                },
+                {
+                    src: "assets/img/portfolio/portfolio-1-details-5.jpg",
+                    alt: ""
+                },
+                {
+                    src: "assets/img/portfolio/portfolio-1-details-6.jpg",
+                    alt: ""
+                }
+            ]
+        },
+        skills: [
+            "Joomla",
+            "Javascript",
+            "Animations with js",
+            "Figma",
+            "illustrator",
+            "Google Scripts",
+        ]
+    },
+    'pacman-game': {
+        description: "pacman game",
+        summary: [
+            { client: "Universidad Nacional de Colombia" },
+            { date: "From August to December, 2019" },
+            { url: ""}
+        ],
+        projectDescription: `
+            This web page is designed web design and custom-designed. It helped the ITAWA BAND think about it presence online. I am excited about this web page, and I'm happy about the results.
+        `,
+        media: {
+            type: "image",
+            items: [
+                {
+                    src: "assets/img/portfolio/portfolio-5-details-1.jpg",
+                    alt: ""
+                },
+                {
+                    src: "assets/img/portfolio/portfolio-5-details-2.jpg",
+                    alt: ""
+                },
+                {
+                    src: "assets/img/portfolio/portfolio-5-details-3.jpg",
+                    alt: ""
+                },
+                {
+                    src: "assets/img/portfolio/portfolio-5-details-4.jpg",
+                    alt: ""
+                },
+                {
+                    src: "assets/img/portfolio/portfolio-5-details-5.jpg",
+                    alt: ""
+                },
+                {
+                    src: "assets/img/portfolio/portfolio-5-details-6.jpg",
+                    alt: ""
+                },
+                {
+                    src: "assets/img/portfolio/portfolio-5-details-7.jpg",
+                    alt: ""
+                },
+                {
+                    src: "assets/img/portfolio/portfolio-5-details-8.jpg",
+                    alt: ""
+                },
+                {
+                    src: "assets/img/portfolio/portfolio-5-details-9.jpg",
+                    alt: ""
+                }
+            ]
+        },
+        skills: [
+            "C++",
+            "OOP",
+            "Agile Methodologies",
+            "Team work",
+        ]
+    },
+    'deleite-website': {
+        description: "Deleite website",
+        summary: [
+            { client: "Deleite Restaurant" },
+            { date: "From March to November, 2021" },
+            { url: ""}
+        ],
+        projectDescription: `
+            This web page is designed with responsive web design and custom-designed. It helped the ITAWA BAND think about it presence online. I am excited about this web page, and I'm happy about the results.
+        `,
+        media: {
+            type: "image",
+            items: [
+                {
+                    src: "assets/img/portfolio/portfolio-1-details-1.jpg",
+                    alt: ""
+                },
+                {
+                    src: "assets/img/portfolio/portfolio-1-details-2.jpg",
+                    alt: ""
+                },
+                {
+                    src: "assets/img/portfolio/portfolio-1-details-3.jpg",
+                    alt: ""
+                },
+                {
+                    src: "assets/img/portfolio/portfolio-1-details-4.jpg",
+                    alt: ""
+                },
+                {
+                    src: "assets/img/portfolio/portfolio-1-details-5.jpg",
+                    alt: ""
+                },
+                {
+                    src: "assets/img/portfolio/portfolio-1-details-6.jpg",
+                    alt: ""
+                }
+            ]
+        },
+        skills: [
+            "HTML",
+            "CSS",
+            "Javascript",
+            "Frontend Basic Principies",
+            "Agile Methodologies"
+        ]
+    },
+};
